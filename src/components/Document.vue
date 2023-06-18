1dae8cd0900221e8a6c75f33d3ebefa922f7bad7
tamaru <on.101.tamaru.taichi@gmail.com>
2023-06-19 08:18:19 +0900

    サブタイトルコンポーネントの作成、各コンポーネントのレイアウト修正

--- src/components/Document.vue
+++ src/components/Document.vue
@@ -8,14 +8,19 @@
 
 <style scoped>
 .fancy-document {
-  background-color: #f5f5f5;
-  color: #333;
-  padding: 20px;
+  background-color: #ffffff;
+  padding: 40px;
+  margin: 20px 50px;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 .content {
   margin-bottom: 20px;
+  color: #333;
+}
+
+.markup-word {
+  color: rgb(37, 139, 255);
 }
 </style>
