d9a1e79ceec703b0dbc927b06074f3237421cd54
日向一樹 <twumooo0626@gmail.com>
2023-06-21 23:36:46 +0900

    💄 コースページのデザイン追加

--- src/components/course/course-2/CommandLineMenuPage.vue
+++ src/components/course/course-2/CommandLineMenuPage.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <h1>Command Lineを選ぼう</h1>
+  <div class="container">
+    <h1 class="header">Command Lineを選ぼう</h1>
     <ul class="lesson-list">
-      <li class="lesson-item"><a class="lesson-link" @click="goToPageLesson1">Command Line 1</a></li>
+      <li class="lesson-item">
+        <a class="lesson-link btn" @click="goToPageLesson1">Command Line 1</a>
+      </li>
     </ul>
   </div>
 </template>
@@ -16,3 +18,59 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.container {
+  width: 100%;
+  margin: 0 auto;
+  padding: 1em;
+  max-width: 600px;
+}
+
+.header {
+  color: #2c3e50;
+  text-align: center;
+  font-size: 2.5em; /* This will make the header bigger */
+}
+
+.lesson-list {
+  display: grid;
+  justify-content: center;
+  list-style: none;
+  padding-left: 0;
+}
+
+.lesson-item {
+  margin-bottom: 1em;
+}
+
+.lesson-link {
+  text-decoration: none;
+  color: #3498db;
+  padding: 0.8em 2em;
+  background-color: #f8f9fa;
+  border: 1px solid #3498db;
+  border-radius: 5px;
+  display: inline-block;
+  transition: background-color 0.3s;
+  position: relative;
+}
+
+.lesson-link:after {
+  content: "";
+  width: 7px;
+  height: 7px;
+  display: inline-block;
+  border-top: 1px solid;
+  border-right: 1px solid;
+  right: 15px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%) rotate(45deg);
+}
+
+.lesson-link:hover {
+  background-color: #3498db;
+  color: #fff;
+}
+</style>
--- src/components/course/course-3/GitMenuPage.vue
+++ src/components/course/course-3/GitMenuPage.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <h1>Gitコースを選ぼう</h1>
+  <div class="container">
+    <h1 class="header">Gitコースを選ぼう</h1>
     <ul class="lesson-list">
-      <li class="lesson-item"><a class="lesson-link" @click="goToPageLesson1">Git 1</a></li>
-      <li class="lesson-item"><a class="lesson-link" @click="goToPageLesson2">Git 2</a></li>
-      <li class="lesson-item"><a class="lesson-link" @click="goToPageLesson3">Git 3</a></li>
+      <li class="lesson-item"><a class="lesson-link btn_22" @click="goToPageLesson1">GitLesson1</a></li>
+      <li class="lesson-item"><a class="lesson-link btn_22" @click="goToPageLesson2">GitLesson2</a></li>
+      <li class="lesson-item"><a class="lesson-link btn_22" @click="goToPageLesson3">GitLesson3</a></li>
     </ul>
   </div>
 </template>
@@ -24,3 +24,91 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  padding: 1em;
+  max-width: 600px;
+}
+
+.header {
+  color: #2c3e50;
+  text-align: center;
+  font-size: 2.5em;
+}
+
+.lesson-list {
+  list-style: none;
+  padding-left: 0;
+}
+
+.lesson-item {
+  margin-bottom: 30px;
+}
+
+.lesson-link {
+  position: relative;
+}
+
+.lesson-link:after {
+  content: "";
+  width: 7px;
+  height: 7px;
+  display: inline-block;
+  border-top: 1px solid;
+  border-right: 1px solid;
+  right: 15px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%) rotate(45deg);
+}
+
+a.btn_22 {
+  display: block;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+  width: 160px;
+  margin: auto;
+  padding: 0.8rem 0;
+  font-weight: bold;
+  border: 2px solid #27acd9;
+  background: #27acd9;
+  color: #fff;
+  border-radius: 5px;
+  transition: 0.5s;
+  animation: move_d 2s infinite;
+  box-shadow: 0 5px 0 rgb(6, 134, 178, 1);
+  position: relative;
+}
+
+@keyframes move_d {
+  0% {
+    box-shadow: 0 5px 0 rgb(6, 134, 178, 1);
+    top: 0px;
+  }
+  10% {
+    box-shadow: 0 0 0 rgb(6, 134, 178, 1);
+    top: 5px;
+  }
+  20% {
+    box-shadow: 0 5px 0 rgb(6, 134, 178, 1);
+    top: 0px;
+  }
+  30% {
+    box-shadow: 0 0 0 rgb(6, 134, 178, 1);
+    top: 5px;
+  }
+  40% {
+    box-shadow: 0 5px 0 rgb(6, 134, 178, 1);
+    top: 0px;
+  }
+}
+
+a.btn_22:hover {
+  color: #27acd9;
+  background: #fff;
+  border: 2px solid #0686b2;
+}
+</style>
