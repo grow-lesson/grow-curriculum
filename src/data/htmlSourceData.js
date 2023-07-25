eacedb253b2d9c4144810e7fc931ce5cd3c3caeb
grow-lesson <grow.lesson.info@gmail.com>
2023-07-25 17:14:02 +0900

    ターミナルのデータを追加

--- src/data/htmlSourceData.js
+++ src/data/htmlSourceData.js
@@ -37,5 +37,24 @@ export const htmlSourceData = {
     `,
     language: "language-html",
   },
+  file3: {
+    filename: "index.html",
+    code: `
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <title>html-css-lesson-3</title>
+  </head>
+  <body>
+
+  </body>
+</html>
+    `,
+    language: "language-html",
+  },
   // 他のデータを追加できます
 };
