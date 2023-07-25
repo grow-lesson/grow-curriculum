e78089cd9f57302c0a2b91525d61287f8932e09d
grow-lesson <grow.lesson.info@gmail.com>
2023-07-25 17:13:26 +0900

    共通フォントの設定

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,15 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 /* Global Styles */
 body {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans', sans-serif;
   line-height: 1.6;
   background-color: #f5f5f5;
   color: #2c3e50;
