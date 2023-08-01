8a81715718f5d941b7a789085690622abe4eac22
grow-lesson <grow.lesson.info@gmail.com>
2023-08-01 15:36:21 +0900

    準備中コンポーネント実装

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header />
+    <div class="under-construction">
+      <h1>準備中...</h1>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./layout/Header.vue";
+import Footer from "./layout/Footer.vue";
+
+export default {
+  name: "UnderConstruction",
+  components: {
+    Header,
+    Footer,
+  },
+}
+</script>
+
+<style>
+.under-construction {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-size: 24px;
+}
+
+h1 {
+  font-size: 48px;
+  margin-bottom: 16px;
+}
+</style>
