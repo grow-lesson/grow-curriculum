ba00d1e6d6582f4cc3230bd31fb13d6b3a64f5ba
grow-lesson <grow.lesson.info@gmail.com>
2023-08-01 15:36:34 +0900

    戻るボタンの実装

---
+++ src/components/BackButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="back-button" @click="goBack">
+    <i class="fa fa-arrow-left"></i> 戻る
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      // ブラウザの履歴を1つ戻る
+      window.history.back();
+    },
+  },
+};
+</script>
+
+<style>
+/* 画面を戻るボタンのスタイル */
+.back-button {
+  display: inline-flex;
+  align-items: center;
+  font-size: 18px;
+  padding: 8px 16px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.back-button i {
+  margin-right: 8px;
+}
+</style>
