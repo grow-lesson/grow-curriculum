e74ad8d8d5aad1dfb858488ec0de26b2b641d8f6
tamaru <on.101.tamaru.taichi@gmail.com>
2023-08-21 08:49:22 +0900

    認証トークンの追加

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL // VUE_APP_API_URL は環境変数から取得されます
+  baseURL: process.env.VUE_APP_API_URL
+});
+
+// 認証トークンをヘッダーに含める
+instance.interceptors.request.use(config => {
+  const token = process.env.VUE_APP_AUTH_TOKEN; // ここに認証トークンを設定
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default instance;
