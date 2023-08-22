20ef3a93556adcfd4082638011d47a99a28eb52d
grow-lesson <grow.lesson.info@gmail.com>
2023-08-22 15:44:24 +0900

    ベースURLをハードコーディング

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL
+  baseURL: 'https://grow-curriculum-backend-f10ce9239245.herokuapp.com'
 });
 
 export default instance;
