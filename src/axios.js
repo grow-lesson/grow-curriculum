960427629bbc5d2366a2b4b99aa864ba325c3ffe
grow-lesson <grow.lesson.info@gmail.com>
2023-08-22 17:48:26 +0900

    originの設定

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
-  headers: {
-    Origin: 'https://grow-curriculum-backend-f10ce9239245.herokuapp.com'
-  }
 });
 
 export default instance;
