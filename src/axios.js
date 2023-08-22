cc203b4037f510c59dc8de21bca7754197436e6e
grow-lesson <grow.lesson.info@gmail.com>
2023-08-22 13:35:42 +0900

    不要な処理の削除

--- src/axios.js
+++ src/axios.js
@@ -4,13 +4,4 @@ const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL
 });
 
-// 認証トークンをヘッダーに含める
-instance.interceptors.request.use(config => {
-  const token = localStorage.getItem('authToken'); // ローカルストレージからトークンを取得
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
 export default instance;
