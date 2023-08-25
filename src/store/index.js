8d7519136799fcf3c694e40331034a1ebbfce55c
grow-lesson <grow.lesson.info@gmail.com>
2023-08-25 16:33:30 +0900

    store/index.jsの修正

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
-// src/store/index.js
-import Vue from 'vue';
-import Vuex from 'vuex';
+import { createStore } from "vuex";
 import userModule from './modules/user'; // ユーザーモジュールのインポート
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
+const store = createStore({
   modules: {
     user: userModule, // ユーザーモジュールを追加
     // 他のモジュールも同様に追加可能
   },
 });
+
+export default store;
