c6a6291b3e0453046029433309d2e71401a4a292
tamaru <on.101.tamaru.taichi@gmail.com>
2023-11-04 22:15:04 +0900

    お問い合わせページのvue3リファクタリングとデザインの修正

--- src/components/others/Contact.vue
+++ src/components/others/Contact.vue
@@ -2,33 +2,31 @@
   <div>
     <Header />
     <div class="contact-form">
-      <h1>お問い合わせ</h1>
-      <form @submit.prevent="submitForm">
-        <div class="form-group">
-          <label for="name">お名前:</label>
-          <input type="text" id="name" v-model="name" required />
+      <h1 class="contact-title">お問い合わせ</h1>
+      <form class="contact-form">
+        <div class="contact-group">
+          <label for="email" class="contact-label">メールアドレス:</label>
+          <input type="email" id="email" class="contact-input" />
         </div>
-        <div class="form-group">
-          <label for="email">メールアドレス:</label>
-          <input type="email" id="email" v-model="email" required />
+        <div class="contact-group">
+          <label for="title" class="contact-label">タイトル:</label>
+          <input type="text" id="title" class="contact-input" />
         </div>
-        <div class="form-group">
-          <label for="message">メッセージ:</label>
-          <textarea id="message" v-model="message" required></textarea>
+        <div class="contact-group">
+          <label for="message" class="contact-label">お問い合わせ内容:</label>
+          <textarea id="message" class="contact-textarea"></textarea>
         </div>
-        <div class="form-group">
-          <button type="submit">送信</button>
+        <div class="contact-group">
+          <button type="submit" class="contact-button">送信</button>
         </div>
       </form>
-      <div v-if="submitted" class="success-message">
-        <p>お問い合わせありがとうございます。ご連絡をお待ちください。</p>
-      </div>
     </div>
     <Footer />
   </div>
 </template>
 
 <script>
+import { onMounted } from 'vue';
 import Header from "@/components/layout/Header.vue";
 import Footer from "@/components/layout/Footer.vue";
 
@@ -37,82 +35,72 @@ export default {
     Header,
     Footer,
   },
-  data() {
+  setup() {
+    onMounted(() => {
+      window.scrollTo(0, 0);
+    });
     return {
-      name: "",
-      email: "",
-      message: "",
-      submitted: false,
     };
-  },
-  methods: {
-    submitForm() {
-      // ここでお問い合わせフォームのデータを送信する処理を行います
-      // 例えば、APIリクエストを実行したり、データベースに保存したりします
-      console.log("お名前:", this.name);
-      console.log("メールアドレス:", this.email);
-      console.log("メッセージ:", this.message);
-
-      // フォームの送信後、submittedフラグをtrueに設定して、送信完了メッセージを表示します
-      this.submitted = true;
-
-      // フォームをリセットします
-      this.resetForm();
-    },
-    resetForm() {
-      this.name = "";
-      this.email = "";
-      this.message = "";
-    },
   },
 };
 </script>
+
 
 <style scoped>
 .contact-form {
   max-width: 400px;
   height: 100vh;
   margin: 0 auto;
+  padding: 20px;
+  background: #f4f4f4;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
-h1 {
+.contact-title {
   text-align: center;
-  margin: 40px 0;
+  font-size: 24px;
+  color: #333;
+  margin: 20px 0;
 }
 
-.form-group {
+.contact-group {
   margin-bottom: 20px;
 }
 
-label {
+.contact-label {
   display: block;
   font-weight: bold;
   margin-bottom: 5px;
+  color: #333;
 }
 
-input,
-textarea {
+.contact-input,
+.contact-textarea {
   width: 100%;
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 4px;
+  font-size: 16px;
 }
 
-button {
+.contact-button {
   padding: 10px 20px;
-  background-color: #4caf50;
+  background-color: #0071bb;
   color: #fff;
   border: none;
   border-radius: 4px;
   cursor: pointer;
+  font-size: 16px;
 }
 
-.success-message {
+.contact-success-message {
   margin-top: 20px;
   padding: 10px;
   background-color: #dff0d8;
   border: 1px solid #d0e9c6;
   border-radius: 4px;
   color: #3c763d;
+  font-size: 16px;
 }
 </style>
