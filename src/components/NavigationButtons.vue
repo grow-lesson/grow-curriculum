2723f7e6de1c42335ba0c7338f02206c5713128a
tamaru <on.101.tamaru.taichi@gmail.com>
2024-05-18 03:15:56 +0900

    ページネーションボタンのスタイル変更

--- src/components/NavigationButtons.vue
+++ src/components/NavigationButtons.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navigation-buttons">
-    <button @click="goToPrevious">←</button>
-    <button @click="goToNext">→</button>
+    <button @click="goToPrevious"><span class="arrow-left"></span></button>
+    <button @click="goToNext"><span class="arrow-right"></span></button>
   </div>
 </template>
 
@@ -31,18 +31,39 @@ export default {
 }
 
 .navigation-buttons button {
-  padding: 10px 20px;
-  font-size: 16px;
+  width: 70px;
+  height: 40px;
   background-color: #3fa0fa;
   color: #ffffff;
   border: none;
-  border-radius: 5px;
+  border-radius: 7px; /* 四角いボタン */
   cursor: pointer;
   transition: background-color 0.3s ease;
+  position: relative;
 }
 
 .navigation-buttons button:hover {
   background-color: #ff9900;
 }
-</style>
 
+.arrow-left, .arrow-right {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 0;
+  height: 0;
+  border-style: solid;
+}
+
+.arrow-left {
+  border-width: 10px 20px 10px 0;
+  border-color: transparent #ffffff transparent transparent;
+  left: 20px; /* 矢印の位置 */
+}
+
+.arrow-right {
+  border-width: 10px 0 10px 20px;
+  border-color: transparent transparent transparent #ffffff;
+  right: 20px; /* 矢印の位置 */
+}
+</style>
