85e82ef05344d483146322e40188ba71cbdf18df
tamaru <on.101.tamaru.taichi@gmail.com>
2024-06-13 00:00:03 +0900

    ログイン機能の実装

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,8 @@ const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
 
+console.log('API Base URL:', process.env.VUE_APP_API_URL);  // 環境変数をログ出力
+
 // リクエストインターセプター
 instance.interceptors.request.use(request => {
   console.log('Starting Request', request);
@@ -18,6 +20,15 @@ instance.interceptors.response.use(
   },
   error => {
     console.error('Response Error:', error);
+    if (error.response) {
+      console.error('Response data:', error.response.data);
+      console.error('Response status:', error.response.status);
+      console.error('Response headers:', error.response.headers);
+    } else if (error.request) {
+      console.error('Request data:', error.request);
+    } else {
+      console.error('Error message:', error.message);
+    }
     return Promise.reject(error);
   }
 );
