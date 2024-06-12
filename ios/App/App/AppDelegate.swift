af5309f85a5c4199e5de0303d5f68726201a2ae2
tamaru <on.101.tamaru.taichi@gmail.com>
2024-06-12 17:53:25 +0900

    スマホアプリの設定

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Alamofire
 import Capacitor
 
 @UIApplicationMain
@@ -8,42 +7,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
-    func login(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
-        let loginData: [String: Any] = [
-            "email": email,
-            "password": password
-        ]
+    func applicationWillResignActive(_ application: UIApplication) {}
 
-        let url = "https://grow-curriculum-backend-f10ce9239245.herokuapp.com/auth/sign_in"
+    func applicationDidEnterBackground(_ application: UIApplication) {}
 
-        AF.request(url, method: .post, parameters: loginData, encoding: JSONEncoding.default).responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                if let json = value as? [String: Any], let status = json["status"] as? String, status == "success" {
-                    if let headers = response.response?.allHeaderFields as? [String: String] {
-                        self.setCookies(headers: headers)
-                    }
-                    completion(.success(()))
-                } else {
-                    completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "ログインエラーが発生しました"])))
-                }
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
-    private func setCookies(headers: [String: String]) {
-        if let accessToken = headers["access-token"],
-           let client = headers["client"],
-           let uid = headers["uid"] {
-            UserDefaults.standard.set(accessToken, forKey: "access-token")
-            UserDefaults.standard.set(client, forKey: "client")
-            UserDefaults.standard.set(uid, forKey: "uid")
-        }
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 }
