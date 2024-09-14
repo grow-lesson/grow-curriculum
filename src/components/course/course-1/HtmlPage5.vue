815be8bf5caa2d990853f2eba05d875af81ec596
土屋暖乃 <Tsuchiya.Tsuchiya.917@gmail.com>
2024-09-14 17:23:47 +0900

    position,BEM設計

--- src/components/course/course-1/HtmlPage5.vue
+++ src/components/course/course-1/HtmlPage5.vue
@@ -11,21 +11,106 @@
         <SubTitle subTitle="1. BEM設計とは？" id="subTitle1"></SubTitle>
 
         <Document>
-          <SectionTitle sectionTitle="1-1. タイトル①" id="sectionTitle1-1"></SectionTitle>
+          <SectionTitle sectionTitle="1-1. BEM設計について" id="sectionTitle1-1"></SectionTitle>
           <p>
-            <HighlightWord word="BEM"></HighlightWord><br>
-            ・BEM設計とは○○です。
+            <HighlightWord word="BEMとは"></HighlightWord><br>
+            BEMとはBlock、Element、Modifierの略語です。class名の命名ルールが特徴的なフロントエンド設計方法の一つです。<br><br>
           </p><br>
-          <Terminal :fileName="htmlSourceData.file9.filename" :copiedText="htmlSourceData.file9.code" :lang="htmlSourceData.file9.language" />
-          <br>
+          <p>
+            <HighlightWord word="BEMの基本形"></HighlightWord><br>
+            基本的なBEMの形は以下のようになっています。<br>
+          </p><br>
+          <div>
+            <img @click="openModal(imagePaths.step2)" :src="imagePaths.step2" class="screen-image" alt="BEM" />
+          </div><br>
+
+          <SectionTitle sectionTitle="1-2. BEM設計を使う目的" id="sectionTitle1-2"></SectionTitle>
+          <p>
+            BEM設計を使うと保守性が高くなります。命名を規則化することによりBlock要素が一つになり、各要素ごとに追加・編集ができるようになります。<br>
+            sassやscssを使ったことがある方であればわかると思いますが、下の画像のように"header"の中の"content"に装飾をつけたいときにはこのように記述することができます。<br>
+            特にcontentやtextやtitleなどは一つのページで複数クラス名に使いたい場面もあるので、そういったときに装飾しやすくなります。<br><br>
+            また、BEM設計は一つ一つのBlockを起点としているので同じページ内でコンテンツを移動したときや、同じコンテンツ(例えば同じデザインのボタンや矢印アイコンなど)を追加したときに影響を受けにくくなります。<br><br>
+          </p>
           <div>
             <img @click="openModal(imagePaths.step1)" :src="imagePaths.step1" class="screen-image" alt="vscode-1" />
           </div><br>
+          <p>
+            この画像のl-についてはこの次のセクションで解説します。
+          </p>          
+        </Document>
+
+        <SubTitle subTitle="2. 接頭辞とは？" id="subTitle2"></SubTitle>
+
+        <Document>
+          <SectionTitle sectionTitle="2-1. 接頭辞について" id="sectionTitle2-1"></SectionTitle>
+          <p>
+            <HighlightWord word="接頭辞とは"></HighlightWord><br>
+            接頭辞とは簡単に言えばクラス名の一番最初につけるもので、おおまかな構造のどれに属するものなのかを定義することができます。<br>
+            このカリキュラムではFLOCSSを使ってコーディングをするため、FLOCSSについても触れながら解説していきます。
+          </p><br>
+          <div>
+            <img @click="openModal(imagePaths.step4)" :src="imagePaths.step4" class="screen-image" alt="settouji" />
+          </div><br>
+          <p>
+            <HighlightWord word="接頭辞・FLOCSSについて"></HighlightWord><br>
+            FLOCSSはWebサイトやWebアプリケーションのスタイルを効率的に構築・管理するためのCSS設計手法です。<br>
+            FLOCSSでは、CSSのクラスを役割に応じて「Foundation」「Layout」「Object」「Component」「Utility」の5つのカテゴリに分類します。<br>
+            接頭辞は各カテゴリのクラス名に貫性を持たせるために用いるもので、接頭辞を使用することで、そのクラスがどの機能や役割を持つかが一目で分かるようになります。<br><br>
+          </p><br>
+
+          <SectionTitle sectionTitle="2-2. 接頭辞の使い方" id="sectionTitle2-2"></SectionTitle>
+          <p>
+            接頭辞の中でもよく使う5つを解説します。<br>
+            <HighlightWord word="Foundation"></HighlightWord><br>
+            Foundationは、ページ全体に影響を与える基本的なスタイルを定義します。このカテゴリのスタイルには通常、接頭辞をつけません。<br>
+            例：html,body,h1,pなどのサイト全体のデフォルトの要素<br><br>
+          </p>
+          <p>
+            <HighlightWord word="Layout"></HighlightWord><br>
+            レイアウトに関連するクラスには、l-という接頭辞が付けられます。Layoutクラスは主にページのグリッドやコンテナなどの大きな構造要素に使用されます。<br>
+            例：l-header,l-main,l-hooter <br>
+            主にページ全体の構造や大枠のレイアウトにつける<br><br>
+          </p>
+          <p>
+            <HighlightWord word="Component"></HighlightWord><br>
+            Componentは、独立したUIパーツに使われます。ボタンやカード、フォーム等にc-という接頭辞をつけます。<br>
+            例：c-button,c-card<br>
+            UIコンポーネントのデザインを定義し、独立で機能するUI要素を作成するためのスタイル。<br><br>
+          </p>
+          <p>
+            <HighlightWord word="Project"></HighlightWord><br>
+            Projectは、コンポーネントの集まりや固有のパターンのスタイルを設定します。<br>
+            例：p-mv,p-contact<br>
+            固有のパターンという言葉では想像しづらいですがメインビジュアルや記事一覧、スライダーなどのコンテンツ全般を定義するので一番利用するものとなっています。<br><br>
+          </p>
+          <p>
+            <HighlightWord word="Utility"></HighlightWord><br>
+            Utilitynは、簡単なスタイル変更や調整を行うために使用されます。u-等言う接頭辞がつけられ、一時的な修正に使われます。<br>
+            例：u-text-center,u-margin<br>
+            特定のスタイルを素早く適用することができ、コンポーネントやオブジェクトに対して小規模な修正や補助を行えるもの。<br><br>
+          </p>
+          <br><br>
+          <div>
+            <img @click="openModal(imagePaths.step3)" :src="imagePaths.step3" class="screen-image" alt="FLOCSS" />
+          </div><br>
+        </Document>
+
+
+        <Document>
+          <SectionTitle sectionTitle="BEM設計・FLOCSS・接頭辞についてまとめ" id="sectionTitle3"></SectionTitle>
+          <p>
+            BEM設計・FLOCSS・接頭辞についての解説でしたが、これらを組み合わせてクラス命名をすることで、よりコードが読みやすく、cssを操作するときに拡張及び保守がしやすくなります。<br>
+            チームでの開発をする際にもこういった命名規則は使われるので、普段から使うように心がけていきましょう。
+          </p>
+          
+          <br><br>
+
           <NavigationButtons
             :previousPageName="'HtmlPage4'"
             :nextPageName="'HtmlPage1'"
           />
         </Document>
+
         <Modal :selectedImage="selectedImage" :showModal="isModalOpen" @closeModal="isModalOpen = false" />
       </div>
       <div class="side-content">
@@ -45,7 +130,7 @@ import SubTitle from "@/components/SubTitle.vue";
 import SectionTitle from "@/components/SectionTitle.vue";
 import HighlightWord from "@/components/HighlightWord.vue";
 import Document from "@/components/Document.vue";
-import Terminal from "@/components/Terminal.vue";
+// import Terminal from "@/components/Terminal.vue";
 import TableOfContents from "@/components/TableOfContents.vue";
 import { htmlSourceData } from "@/data/htmlSourceData.js";
 import Modal from "@/components/common/Modal.vue";
@@ -61,7 +146,7 @@ export default {
     SectionTitle,
     HighlightWord,
     Document,
-    Terminal,
+    // Terminal,
     TableOfContents,
     Modal,
     NavigationButtons,
@@ -70,7 +155,11 @@ export default {
     const sections =  [
       { id: "Title", title: "HTML・CSS 5" },
       { id: "subTitle", title: "1. BEM設計とは？" },
-      { id: "sectionTitle1-1", title: "    1-1. タイトル①" },
+      { id: "sectionTitle1-1", title: "    1-1. BEM設計について" },
+      { id: "sectionTitle1-2", title: "    1-2. BEM設計を使う目的" },
+      { id: "subTitle", title: "2. 接頭辞とは？" },
+      { id: "sectionTitle2-1", title: "    2-1. 接頭辞について" },
+      { id: "sectionTitle2-1", title: "    2-2. 接頭辞の使い方" },
     ];
 
     onMounted(() => {
@@ -78,7 +167,10 @@ export default {
     });
 
     const imagePaths = {
-      step1: require("../../../assets/images/course/html-css-5/dog.jpg"),
+      step1: require("../../../assets/images/course/html-css-5/vscode-1.png"),
+      step2: require("../../../assets/images/course/html-css-5/BEM.jpg"),
+      step3: require("../../../assets/images/course/html-css-5/FLOCSS.jpg"),
+      step4: require("../../../assets/images/course/html-css-5/settouji.jpg"),
     };
 
     // モーダル関連のデータ
